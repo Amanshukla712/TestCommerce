d7806a6a7b5ebaa76b94700e75ab6e8278015e70
Amanshukla712 <120775204+Amanshukla712@users.noreply.github.com>
2024-07-14 17:31:27 +0530

    Added files from PC

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Products from "./components/Products/products";
+import Header from "./components/Layout/Header";
+import SubHeader from "./components/Layout/SubHeader";
+
+
+const App = () => {
+
+  return (
+    <div >
+      <Header/>
+      <SubHeader/>
+      <Products />
+    </div>
+  );
+}
+
+export default App;
